e8b2da14b7ac440bcde93def658b4d698b42d4fd
vvasa33 <vvasa33@icloud.com>
2023-10-31 11:01:28 -0400

    before lunch, finished all cases + pidf control for ground case

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Subsystems/Lift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Subsystems/Lift.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.Hardware.Subsystems;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 
 import org.firstinspires.ftc.teamcode.Hardware.HardwareConstants;
 import org.firstinspires.ftc.teamcode.Hardware.HardwareConstants.*;
@@ -13,13 +14,14 @@ public class Lift {
 
     LinearOpMode opMode;
 
-    PIDFController controller;
 
+    PIDFController liftController;
 
 
     public Lift(LinearOpMode opMode) {
         this.opMode = opMode;
         lift = opMode.hardwareMap.get(DcMotorEx.class, "lift");
+        liftController = new PIDFController(lift, new PIDFCoefficients(0,0,0,0));
     }
 
     public void controlLift() {
@@ -28,13 +30,28 @@ public class Lift {
                 if (opMode.gamepad2.b) {
                     setLiftParameters(LiftStates.SETLINE_1.getValue(), DcMotor.RunMode.RUN_TO_POSITION, HardwareConstants.liftSpeed);
                     HardwareConstants.currentLiftState = LiftStates.SETLINE_1;
+                } else if (opMode.gamepad2.x) {
+                    setLiftParameters(LiftStates.SETLINE_2.getValue(), DcMotor.RunMode.RUN_TO_POSITION, HardwareConstants.liftSpeed);
+                    HardwareConstants.currentLiftState = LiftStates.SETLINE_2;
+                } else if (opMode.gamepad2.y) {
+                    setLiftParameters(LiftStates.SETLINE_3.getValue(), DcMotor.RunMode.RUN_TO_POSITION, HardwareConstants.liftSpeed);
+                    HardwareConstants.currentLiftState = LiftStates.SETLINE_3;
+                } else {
+                    setLiftParameters();
                 }
+            case SETLINE_1:
+
         }
     }
 
     public void setLiftParameters(int position, DcMotor.RunMode runmode, double power) {
         lift.setTargetPosition(position);
         lift.setMode(runmode);
-        lift.setPower(power);
+        lift.setPower(liftController.calculate(position));
+    }
+
+    public void setLiftParameters() {
+        lift.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        lift.setPower(0);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utilities/PIDFController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utilities/PIDFController.java
@@ -6,22 +6,22 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 public class PIDFController {
     private double kp, ki, kd, kf;
-    private final double reference;
+
     private double integralSum = 0;
     private double lastError = 0;
     ElapsedTime timer = new ElapsedTime();
     private final DcMotorEx motor;
 
-    public PIDFController(DcMotorEx motor, PIDFCoefficients coefficients, double reference) {
+    public PIDFController(DcMotorEx motor, PIDFCoefficients coefficients) {
         this.motor = motor;
         coefficients.p = kp;
         coefficients.i = ki;
         coefficients.d = kd;
         coefficients.f = kf;
-        this.reference = reference;
+
     }
 
-    public double calculate() {
+    public double calculate(double reference) {
         double position = motor.getCurrentPosition();
         double error = reference - position;
         //velocityError = targetVelocity - robotCurrentVelocity;
