fa1a874af12b7f3202f7b503a928729d03bae47d
vvasa33 <vvasa33@icloud.com>
2023-11-06 12:18:09 -0500

    whoops i forgort something

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/ExampleStuff/FieldCentricDriveRoadrunner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/ExampleStuff/FieldCentricDriveRoadrunner.java
@@ -15,7 +15,7 @@ public class FieldCentricDriveRoadrunner extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         drive = new SampleMecanumDrive(hardwareMap);
         drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        drive.setPoseEstimate(LocalizationStorageCloser.currentPose);
+        drive.setPoseEstimate(LocalizationStorageCloser.poseIntoTeleOp);
 
         waitForStart();
 
