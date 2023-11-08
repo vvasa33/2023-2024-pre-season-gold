eb264c7e9c0bd23a6ebd00a45cbf037903b6f673
vvasa33 <vvasa33@icloud.com>
2023-11-08 07:13:31 -0500

    started working on paths for leftcloseauto

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -10,20 +10,23 @@ import java.util.Vector;
 
 public class MeepMeepTesting {
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         MeepMeep meep = new MeepMeep(800);
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 14.65)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(6.2, 62.7, Math.toRadians(90)))
-                                .lineTo(new Vector2d(22.7,41.6)) //go to the left spike mark
-                                .splineToLinearHeading(new Pose2d(49,41.4, Math.toRadians(180)), Math.toRadians(0)) //go to the left backdrop
-                                .splineTo(new Vector2d(10,10), Math.toRadians(180)) //go to the beginning of the stage door
-                                .splineTo(new Vector2d(-35.6, 10), Math.toRadians(180)) //go through the door
-                                .splineTo(new Vector2d(-58, 11.8), Math.toRadians(180)) //go to the left pixel stack
-                                .splineToLinearHeading(new Pose2d(10, 10, 0), Math.toRadians(0)) //go back
+                        drive.trajectorySequenceBuilder(new Pose2d(14.65/2, 62.7, Math.toRadians(90)))
+                                .lineTo(new Vector2d(22.7,46.4)) //vision spike left
+                                .splineToLinearHeading(new Pose2d(47.1, 41.1, Math.toRadians(180)), Math.toRadians(0)) //board spot left
+                                .splineTo(new Vector2d(10,10), Math.toRadians(180)) //align with the backstage door
+                                .splineTo(new Vector2d(-36.3, 13), Math.toRadians(180)) //go through door
+                                .splineTo(new Vector2d(-58, 11.8), Math.toRadians(180)) //go to left pixel stack
+                                .splineToConstantHeading(new Vector2d(-36.3, 13), Math.toRadians(0)) //line back up with the backstage door
+                                .splineToConstantHeading(new Vector2d(10,10), Math.toRadians(0)) //go through door again
+                                .splineTo(new Vector2d(47.1, 28.4), Math.toRadians(0)) //go to the right scoring location
+                                //NOW WE GO BACK
 
                                 .build()
                 );
