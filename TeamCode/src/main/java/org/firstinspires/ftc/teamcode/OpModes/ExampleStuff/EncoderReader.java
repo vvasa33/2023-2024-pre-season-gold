1420d5232beb2f212181c447a9ff06a0c6fe7fc4
vvasa33 <vvasa33@icloud.com>
2023-12-08 10:08:04 -0500

    code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/ExampleStuff/EncoderReader.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.OpModes.ExampleStuff;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp (name="Encoder Reader")
+public class EncoderReader extends LinearOpMode {
+    DcMotorEx lift;
+    @Override
+    public void runOpMode() throws InterruptedException {
+        lift = hardwareMap.get(DcMotorEx.class, "lift");
+        lift.setDirection(DcMotorSimple.Direction.REVERSE);
+        lift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        lift.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        lift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        waitForStart();
+
+        while (opModeIsActive() && !isStopRequested()) {
+            telemetry.addData("Encoder ticks", lift.getCurrentPosition());
+            telemetry.update();
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/ExampleStuff/LIftControlWithoutPID.java
@@ -0,0 +1,92 @@
+package org.firstinspires.ftc.teamcode.OpModes.ExampleStuff;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.outoftheboxrobotics.photoncore.PhotonCore;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import org.firstinspires.ftc.teamcode.Hardware.HardwareConstants;
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+
+@TeleOp(name="liftcontrolnopid")
+public class LIftControlWithoutPID extends LinearOpMode {
+    SampleMecanumDrive drive;
+    DcMotorEx lift;
+
+    public static int target;
+    @Override
+    public void runOpMode() throws InterruptedException {
+        lift = hardwareMap.get(DcMotorEx.class, "lift");
+        lift.setDirection(DcMotorSimple.Direction.REVERSE);
+        lift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        lift.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        lift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        //HardwareConstants.currentLiftPosition = HardwareConstants.LiftPositions.GROUND;
+        HardwareConstants.currentLiftState = HardwareConstants.LiftStates.WAITING;
+
+        drive = new SampleMecanumDrive(hardwareMap);
+
+        PhotonCore.experimental.setMaximumParallelCommands(6);
+        PhotonCore.start(hardwareMap);
+
+        waitForStart();
+
+        while (opModeIsActive() && !isStopRequested()) {
+            if (gamepad1.a && HardwareConstants.currentLiftState != HardwareConstants.LiftStates.WAITING) {
+                HardwareConstants.currentLiftState = HardwareConstants.LiftStates.RETRACT;
+            } else if (gamepad1.b) {
+                HardwareConstants.currentLiftState = HardwareConstants.LiftStates.EXTEND;
+                target = HardwareConstants.LiftPositions.SETLINE_1.getValue();
+            } else if (gamepad1.x) {
+                HardwareConstants.currentLiftState = HardwareConstants.LiftStates.EXTEND;
+                target = HardwareConstants.LiftPositions.SETLINE_2.getValue();
+            } else if (gamepad1.y) {
+                HardwareConstants.currentLiftState = HardwareConstants.LiftStates.EXTEND;
+                target = HardwareConstants.LiftPositions.SETLINE_3.getValue();
+            }
+
+            switch (HardwareConstants.currentLiftState) {
+                case WAITING:
+                    break;
+                case EXTEND:
+                    lift.setTargetPosition(target);
+                    lift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    lift.setPower(0.7);
+                    if (!lift.isBusy()) {
+                        HardwareConstants.currentLiftState = HardwareConstants.LiftStates.DEPOSIT;
+                    }
+                    break;
+                case DEPOSIT:
+                    if (HardwareConstants.currentRightClawState == HardwareConstants.ClawStates.OPEN && HardwareConstants.currentLeftClawState == HardwareConstants.ClawStates.OPEN) {
+                        sleep(200); //wait a little bit so that the
+                        HardwareConstants.currentLiftState = HardwareConstants.LiftStates.RETRACT;
+                    }
+                    break;
+
+                case RETRACT:
+                    lift.setTargetPosition(0);
+                    lift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    lift.setPower(0.7);
+                    if (!lift.isBusy()) {
+                        HardwareConstants.currentLiftState = HardwareConstants.LiftStates.WAITING;
+                    }
+                    break;
+            }
+
+            drive.setWeightedDrivePower(
+                    new Pose2d(
+                            -gamepad1.left_stick_y / ((gamepad1.left_bumper) ? 3 : 1),
+                            -gamepad1.left_stick_x / ((gamepad1.left_bumper) ? 3 : 1),
+                            -gamepad1.right_stick_x
+                    )
+            );
+
+            drive.update();
+
+
+        }
+    }
+}
