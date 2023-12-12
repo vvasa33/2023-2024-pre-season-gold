5cae4420eb4e2fd4d966a1b3dcb699487479a2c2
vvasa33 <vvasa33@icloud.com>
2023-12-12 11:00:04 -0500

    new camera code just dropped (i forgot to do the stuff for the camera)

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepCloseRed.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepCloseRed.java
@@ -1,11 +1,12 @@
 package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
-public class MeepMeepFarBlue {
+public class MeepMeepCloseRed {
 
     public static void main(String[] args) {
         MeepMeep meep = new MeepMeep(800);
@@ -13,9 +14,15 @@ public class MeepMeepFarBlue {
         RoadRunnerBotEntity meepmeep = new DefaultBotBuilder(meep)
                 .setConstraints(55, 55, 4.115477328170351, Math.toRadians(194.6310524271845), 14.65)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(-34, 62.7, Math.toRadians(90)))
+                        drive.trajectorySequenceBuilder(new Pose2d((14.65 / 2) + 1, -62.7, Math.toRadians(90)))
+                                .lineTo(new Vector2d(0,0))
                                 .build()
 
                         );
+        meep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_JUICE_DARK)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(meepmeep)
+                .start();
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CameraStuff/AprilTagDetection.java
@@ -0,0 +1,86 @@
+package org.firstinspires.ftc.teamcode.CameraStuff;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagGameDatabase;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@TeleOp (name="AprilTagDetection")
+public class AprilTagDetection extends LinearOpMode {
+    private AprilTagProcessor april;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        telemetry.addData("DS preview on/off", "3 dots, Camera Stream");
+        telemetry.addData(">", "Touch Play to start OpMode");
+        telemetry.update();
+        waitForStart();
+
+        april = new AprilTagProcessor.Builder()
+                .setDrawAxes(true)
+                .setDrawCubeProjection(true)
+                .setDrawTagOutline(true)
+                .setTagFamily(AprilTagProcessor.TagFamily.TAG_36h11)
+                .setTagLibrary(AprilTagGameDatabase.getCenterStageTagLibrary())
+                .setOutputUnits(DistanceUnit.INCH, AngleUnit.DEGREES)
+                .build();
+
+        VisionPortal.Builder builder = new VisionPortal.Builder();
+
+        builder.setCamera(hardwareMap.get(WebcamName.class, "webcam"));
+        builder.addProcessor(april);
+        VisionPortal portal = builder.build();
+
+        waitForStart();
+
+        while (opModeIsActive() && !isStopRequested()) {
+            telemetry();
+
+            telemetry.update();
+
+            if (gamepad1.dpad_down) {
+                portal.stopStreaming();
+            } else if (gamepad1.dpad_up) {
+                portal.resumeStreaming();
+            }
+            sleep(20);
+        }
+
+
+    }
+
+    public void telemetry() {
+        List<org.firstinspires.ftc.vision.apriltag.AprilTagDetection> currentDetections = april.getDetections();
+        telemetry.addData("# AprilTags Detected", currentDetections.size());
+
+
+
+        // Step through the list of detections and display info for each one.
+        for (org.firstinspires.ftc.vision.apriltag.AprilTagDetection detection : currentDetections) {
+            if (detection.metadata != null) {
+                telemetry.addLine(String.format("\n==== (ID %d) %s", detection.id, detection.metadata.name));
+                telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)", detection.ftcPose.x, detection.ftcPose.y, detection.ftcPose.z));
+                telemetry.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)", detection.ftcPose.pitch, detection.ftcPose.roll, detection.ftcPose.yaw));
+                telemetry.addLine(String.format("RBE %6.1f %6.1f %6.1f  (inch, deg, deg)", detection.ftcPose.range, detection.ftcPose.bearing, detection.ftcPose.elevation));
+            } else {
+                telemetry.addLine(String.format("\n==== (ID %d) Unknown", detection.id));
+                telemetry.addLine(String.format("Center %6.0f %6.0f   (pixels)", detection.center.x, detection.center.y));
+            }
+        }
+
+        telemetry.addLine("\nkey:\nXYZ = X (Right), Y (Forward), Z (Up) dist.");
+        telemetry.addLine("PRY = Pitch, Roll & Yaw (XYZ Rotation)");
+        telemetry.addLine("RBE = Range, Bearing & Elevation");
+    }
+
+
+}
